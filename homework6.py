5ce89ac999956bfc1252355427fdb4145f7d1caa
polina-v80 <166666650+polina-v80@users.noreply.github.com>
2024-04-23 16:58:13 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,30 @@
+a, b = 50, 6
+if a > b:
+    print('a > b')
+if a > b and a > 0:
+    print('gud')
+if (a > b) and (a > 0 or b < 1000):
+    print('gud')
+if 3 < b and b < 10:
+    print('gud')
+if '76'>'568':
+    print('ok')
+if '234'>'23':
+    print('OK')
+if [1,2] > [1,1]:
+    print('Ok')
+# a = 5
+# if a == 5 and (a > 1 or a < 10):# сравнение разных типов данных не возможно
+#     print( 'Ok')
+# if '234'>23
+# if [4,5] > 23
+if '234' != 23:
+    print('!')
+
+
+
+# age = int (input('Сколько Вам лет? _'))# input запрос данных у пользователя с клавиатуры
+# if age >= 18:
+#     print('Ok')
+# if age < 18:
+#     print('No')
